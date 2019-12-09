7bd2deacec53c2fab0b44cca5ac1d074a7d7a1be
Gustavo André Fontes Lopes <gflopes@MacBook-Pro-de-Gustavo.local>
2019-12-08 22:39:58 -0300

    Correção de bug no serviço get usuário

--- api/usuario/usuarioService.js
+++ api/usuario/usuarioService.js
@@ -1,89 +1,93 @@
 const _ = require("lodash");
 const Usuario = require("../../model/usuario");
 
-Usuario.methods(['get', 'post', 'put', 'delete'])
+Usuario.methods(["get", "post", "put", "delete"]);
 Usuario.updateOptions({
-    new: true,
-    runValidators: true
-})
-Usuario.after('get', sendErrorsOrNext)
+  new: true,
+  runValidators: true
+});
+Usuario.after("get", sendErrorsOrNext);
 
 // tratamento de erros e parse das mensagens de erro devolvidos
 function sendErrorsOrNext(req, res, next) {
-    const bundle = res.locals.bundle
+  const bundle = res.locals.bundle;
 
-    if (bundle.errors) {
-        var mensagem = parseErrors(bundle.errors)
-        res.status(500).json({
-            mensagem
-        })
-    } else {
-        next()
-    }
+  if (bundle.errors) {
+    var mensagem = parseErrors(bundle.errors);
+    res.status(500).json({
+      mensagem
+    });
+  } else {
+    next();
+  }
 }
 
 function parseErrors(nodeRestfulErrors) {
-    const mensagem = []
-    _.forIn(nodeRestfulErrors, error => mensagem.push(error.message))
-    return mensagem
+  const mensagem = [];
+  _.forIn(nodeRestfulErrors, error => mensagem.push(error.message));
+  return mensagem;
 }
 
 const find = (req, res) => {
-    let token = req.headers['authorization']
-    if (token && token.startsWith('Bearer ')) {
-        token = token.slice(7, token.length);
+  let token;
+  if (req.headers["authorization"] != null) {
+    token = req.headers["authorization"];
+    if (token.startsWith("Bearer ")) {
+      token = token.slice(7, token.length);
     }
+  }
 
-    Usuario.findOne({
-        token: token
-    }, function (err, tokenEncontrado) {
-        if (err) {
-            res.status(500).json({
-                mensagem: err
-            })
+  Usuario.findOne(
+    {
+      token: token
+    },
+    function(err, tokenEncontrado) {
+      if (err) {
+        res.status(500).json({
+          mensagem: err
+        });
+      } else {
+        if (tokenEncontrado == null) {
+          return res.status(401).send({
+            mensagem: "Não autorizado."
+          });
         } else {
-            if (tokenEncontrado == null) {
-                return res.status(401).send({
-                    mensagem: "Não autorizado."
-                })
-            } else {
-                Usuario.findById(req.params.id,
-                    function (err, usuario) {
-                        if (err) {
-                            return res.status(500).send({
-                                mensagem: err
-                            })
-                        }
-
-                        if (usuario.token != token) {
-                            return res.status(401).send({
-                                mensagem: "Não autorizado."
-                            })
-                        } else {
-                            let today = new Date()
-                            let diffMs = (today - usuario.data_ultimo_login);
-                            let diffMins = Math.round((diffMs / 1000) / 60);
-
-                            // console.log("today: " + today)
-                            // console.log("data_ultimo_login: " + usuario.data_ultimo_login)
-                            // console.log("diffMins: " + diffMins)
-
-                            if (diffMins < 30) {
-                                return res.status(401).send({
-                                    mensagem: "Sessão inválida."
-                                })
-                            }
-                            return res.status(200).json({
-                                usuario
-                            })
-                        }
-                    }
-                )
+          Usuario.findById(req.params.id, function(err, usuario) {
+            if (err) {
+              return res.status(500).send({
+                mensagem: err
+              });
             }
+
+            if (usuario.token != token) {
+              return res.status(401).send({
+                mensagem: "Não autorizado."
+              });
+            } else {
+              let today = new Date();
+              let diffMs = today - usuario.data_ultimo_login;
+              let diffMins = Math.round(diffMs / 1000 / 60);
+
+              // console.log("today: " + today)
+              // console.log("data_ultimo_login: " + usuario.data_ultimo_login)
+              // console.log("diffMins: " + diffMins)
+
+              if (diffMins < 30) {
+                return res.status(401).send({
+                  mensagem: "Sessão inválida."
+                });
+              }
+              return res.status(200).json({
+                usuario
+              });
+            }
+          });
         }
-    })
-}
+      }
+    }
+  );
+};
 
 module.exports = {
-    find
+  find
 };
